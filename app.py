2e86a26ce17a16752e82c2a8c581dc1901f4f7f0
rickpetersen <rickpetersen@gmail.com>
2023-06-26 19:49:56 +0000

    Initial Flask template/app

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
