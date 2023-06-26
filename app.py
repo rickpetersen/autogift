82c6bf8786ac12480050b140a04170e3490825fb
rickpetersen <rickpetersen@gmail.com>
2023-06-26 20:37:07 +0000

    starting to add styling

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def friend_and_family_list():
+    return render_template("target_list.html", title="Friends and Family")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
